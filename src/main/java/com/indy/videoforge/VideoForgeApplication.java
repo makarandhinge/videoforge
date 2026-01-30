1b279c1852fc00788953dda74fdf780f2e0f9cb4
Makarand Milind Hinge <mmh2212002@gmail.com>
2026-01-30 22:22:01 +0530

    first commit

---
+++ src/main/java/com/indy/videoforge/VideoForgeApplication.java
@@ -0,0 +1,13 @@
+package com.indy.videoforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoForgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoForgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/indy/videoforge/VideoForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indy.videoforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoForgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
